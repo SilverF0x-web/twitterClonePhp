01da4cbfafe21ca55bf6025fc58ceaab354cae42
SilverF0x-web <lugovnin@inbox.ru>
2021-11-16 15:40:50 +0300

    sign_up + some modifications2

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ include_once "includes/functions.php";
 $posts = get_posts();
 $title = 'Главная страница';
 
-
 include_once "includes/header.php";
 include_once "includes/tweet_form.php";
 include_once "includes/posts.php";
